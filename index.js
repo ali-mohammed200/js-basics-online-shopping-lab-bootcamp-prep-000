3e11e9b76c871495b6461584cda837c0847c83bd
ali.mohammed200 <ali.mohammed200@gmail.com>
2018-11-17 09:30:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,7 +46,14 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for(let i = 0; i < cart.length; i++){
+    let objElem = cart[i];
+    if(objElem["itemName"] === item){
+      cart.splice(i, 1);
+      return cart;
+    }
+  }
+  return `That item is not in your cart.`;
 }
 
 function placeOrder(cardNumber) {
