ea392bb7cc2a90b2ad0666a5aa5f28b3b610b7c2
ali.mohammed200 <ali.mohammed200@gmail.com>
2018-11-17 09:35:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -61,5 +61,6 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   cart = [];
-  return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
+  let amount = total();
+  return `Your total cost is ${amount}, which will be charged to the card ${cardNumber}.`;
 }
